2c24fa31b220ec1b9651f8fc3bf751df0975dc05
Charlie Fang <charlie.fang0130@gmail.com>
2025-01-21 12:39:55 -0500

    Initialize springboot

---
+++ setmate-backend/src/main/java/com/setmate/setmate_backend/SetmateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.setmate.setmate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetmateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetmateBackendApplication.class, args);
+	}
+
+}
---
+++ setmate-backend/src/test/java/com/setmate/setmate_backend/SetmateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setmate.setmate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetmateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
